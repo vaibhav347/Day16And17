1c16b06e22740390e41ec3af0888164a92bebc22
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:46:16 +0530

    ok

---
+++ Day16And17Prime1to1000/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day16And17Prime1to1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.prime();
+        }
+
+        void prime()
+        {
+            int  num = 1;
+            // Console.WriteLine("Enter Input between 1 to 1000");
+            //num = int.Parse(Console.ReadLine());
+            while (num <= 1000)
+            {
+                int flag = 0;
+                for (int i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                    Console.WriteLine("Prime Number : " + num);
+                num++;
+            }
+
+        }
+    }
+}
+    
+
+
