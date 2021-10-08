bd6a1981bcc63e72c0492f1cf66a553d732d8932
Vaibhav Aher1313 <Vaibhav_Aher1313@jabil.com>
2021-10-08 15:49:11 +0530

    ok

---
+++ Sorts/Day16And17BubbleSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Day16And17BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int[] intArray = new int[5];
+            Console.WriteLine("Enter the Array Elements : ");
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+            //Sorting the array
+            for (int j = 0; j <= intArray.Length - 2; j++)
+            {
+                //intArray.Length - 2
+                for (int i = 0; i <= intArray.Length - 2; i++)
+                {
+                    count = count + 1;
+                    if (intArray[i] > intArray[i + 1])
+                    {
+                        int temp = intArray[i + 1];
+                        intArray[i + 1] = intArray[i];
+                        intArray[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After Sorting Array :");
+            foreach (int item in intArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("The Loop iterates :" + count);
+            Console.ReadKey();
+        }
+    }
+}
